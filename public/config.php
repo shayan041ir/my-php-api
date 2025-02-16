e2a023d995d38b43b46314050e485995e1f0331a
shayan041ir <shayanrezayi14@gmail.com>
2025-02-16 04:06:25 +0330

    connect to db

---
+++ public/config.php
@@ -0,0 +1,15 @@
+<php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "myapi_db";
+
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
